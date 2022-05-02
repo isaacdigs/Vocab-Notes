fe2f5a8f1d48764a35a8d11e74d8d7436b169d9e
isaacdigs <pk07057@gmail.com>
2022-05-02 18:53:31 +0900

    First commit - make basic wireframe and classes for app

---
+++ app.js
@@ -0,0 +1,35 @@
+//Vocab class: represents a vocabulary
+class Vocab {
+	constructor(word,meaning,difficulty) {
+		this.word = word;
+		this.meaning = meaning;
+		this.difficulty = difficulty;
+	}
+}
+
+//UI class: handles UI Tasks
+class UI {
+	static displayWords() {
+		//Predifined words for testing
+		const words = [
+			{ word: "somber",
+			meaning: "dark or dull in color or tone; gloomy."
+			difficulty: 3}
+			{ word: "sedentary",
+			meaning: "(of a person) tending to spend much time seated; somewhat inactive."
+			difficulty: 8}
+		]
+	}
+}
+
+
+//Store class: handles storage
+
+//Event : display vocab
+
+//Event : add a vocab
+
+//Event : remove a bocab
+
+
+
